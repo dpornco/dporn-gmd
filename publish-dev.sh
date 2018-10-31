eb95772ee9c99e56499c664c380a45d0067a8bc0
Leather Dog Muksihs <muksihs@gmail.com>
2018-10-30 22:01:19 -0400

    publish script adjustments

--- publish-dev.sh
+++ publish-dev.sh
@@ -6,7 +6,6 @@ set -o pipefail
 cd "$(dirname "$0")"
 
 gradle clean
-gradle compileGwt
 gradle build
 gradle buildProductDev
 
@@ -16,16 +15,13 @@ cd build/output
 
 #do includes *before* excludes
 
-rsync	--include '/webapps/*.*' --exclude '/webapps/*/' \
-		--exclude /work/ \
+#recursive rsync
+rsync	--exclude /work/ \
 		--exclude /temp/ \
-		-e "ssh -p $PORT" --progress --verbose --human-readable --compress --delete-after -a \
+		--progress --verbose --human-readable --compress --delete-after -a \
+		-e "ssh -p $PORT" \
 	Dev/ "$USER"@"$HOST":Dev/
 	
-#rsync	--include '*.*' --exclude '*/' \
-#	-e "ssh -p $PORT" --progress --verbose --human-readable --compress --delete-after -a \
-#	Dev/webapps/ "$USER"@"$HOST":Dev/webapps/
-
 ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; bash stop.sh'
 
 ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; (bash start.sh < /dev/null > /dev/null 2>&1) & sleep 3'
