ae2ff8c87fadcf356dc91ee1a3fa5f019d6bde39
Leather Dog Muksihs <muksihs@gmail.com>
2018-11-03 02:56:19 -0400

    more if modified since changes

--- publish-dev.sh
+++ publish-dev.sh
@@ -15,13 +15,13 @@ cd build/output
 
 #do includes *before* excludes
 
+ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; bash stop.sh' || true
 #recursive rsync
 rsync	--exclude /work/ \
 		--exclude /temp/ \
-		--progress --verbose --human-readable --compress --delete-after -a \
+		--progress --verbose --human-readable --compress --delete-before -a \
 		-e "ssh -p $PORT" \
 	Dev/ "$USER"@"$HOST":Dev/
 	
-ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; bash stop.sh' || true
 
 ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; (bash start.sh < /dev/null > /dev/null 2>&1) & sleep 3' || true
--- src/main/java/co/dporn/gmd/servlet/DpornCoEmbed.java
+++ src/main/java/co/dporn/gmd/servlet/DpornCoEmbed.java
@@ -9,15 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringEscapeUtils;import com.gargoylesoftware.htmlunit.javascript.host.fetch.Request;
+import org.apache.commons.lang3.StringEscapeUtils;
+import com.gargoylesoftware.htmlunit.javascript.host.fetch.Request;
 
 import co.dporn.gmd.shared.Post;
 
@@ -26,79 +29,92 @@ import co.dporn.gmd.shared.Post;
 @Path("/")
 public class DpornCoEmbed {
 	private static String _template;
+
 	private static String template() {
-		if (_template==null) {
+		if (_template == null) {
 			try {
-				_template=IOUtils.toString(DpornCoEmbed.class.getResourceAsStream("/embed/embed-player.html"));
+				_template = IOUtils.toString(DpornCoEmbed.class.getResourceAsStream("/embed/embed-player.html"));
 			} catch (IOException e) {
 			}
 		}
 		return _template;
 	}
-	
+
 	@Context
 	private HttpServletResponse response;
 	@Context
 	private HttpServletRequest request;
-	private static final long LAST_MODIFIED=System.currentTimeMillis();
-	
+	private static final long LAST_MODIFIED = System.currentTimeMillis();
+
 	private static Map<String, String> cache = Collections.synchronizedMap(new HashMap<>());
+
+	@Produces(MediaType.TEXT_HTML)
+	@Path("@{authorname}/{permlink}")
+	@HEAD
+	public Response head(@PathParam("authorname") String author, @PathParam("permlink") String permlink,
+			@QueryParam("base-url") String baseUrl) {
+		String key = author.trim() + "|" + permlink.trim();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.TEXT_HTML);
+		response.addDateHeader("Last-Modified", LAST_MODIFIED);
+		long ifModifiedSince = request.getDateHeader("If-Modified-Since");
+		if (ifModifiedSince >= LAST_MODIFIED) {
+			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+			System.out.print("CLIENT CACHED IFRAME: " + key);
+			return Response.notModified().build();
+		}
+		return Response.ok().build();
+	}
+
 	@Produces(MediaType.TEXT_HTML)
 	@Path("@{authorname}/{permlink}")
 	@GET
-	public String player(@PathParam("authorname") String author, @PathParam("permlink") String permlink, @QueryParam("base-url") String baseUrl) {
-		long ifModifiedSince = request.getDateHeader("If-Modified-Since");
-		if (ifModifiedSince>=0) {
-			if (ifModifiedSince>=LAST_MODIFIED) {
-				response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-		        return "";
-			}
-		}
-		response.addDateHeader("Last-Modified", LAST_MODIFIED);
+	public String get(@PathParam("authorname") String author, @PathParam("permlink") String permlink) {
+		String key = author.trim() + "|" + permlink.trim();
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType(MediaType.TEXT_HTML);
-		String key = author.trim()+"|"+permlink.trim();
+		response.addDateHeader("Last-Modified", LAST_MODIFIED);
 		if (cache.containsKey(key)) {
 			return cache.get(key);
 		}
 		Post post = MongoDpornoCo.getPost(author, permlink);
 		isValid: {
-			if (post==null) {
+			if (post == null) {
 				break isValid;
 			}
 			if (!author.equals(post.getAuthor())) {
 				break isValid;
 			}
 			String coverImageIpfs = post.getCoverImageIpfs();
-			if (coverImageIpfs==null) {
+			if (coverImageIpfs == null) {
 				break isValid;
 			}
-			coverImageIpfs=coverImageIpfs.trim();
-			if (coverImageIpfs.length()!=46) {
+			coverImageIpfs = coverImageIpfs.trim();
+			if (coverImageIpfs.length() != 46) {
 				break isValid;
 			}
 			String videoIpfs = post.getVideoIpfs();
-			if (videoIpfs==null) {
+			if (videoIpfs == null) {
 				break isValid;
 			}
-			videoIpfs=videoIpfs.trim();
-			if (videoIpfs.length()!=46) {
+			videoIpfs = videoIpfs.trim();
+			if (videoIpfs.length() != 46) {
 				break isValid;
 			}
 			String embedHtml = template();
-			embedHtml=embedHtml.replace("__TITLE__", StringEscapeUtils.escapeXml10(post.getTitle()));
-			embedHtml=embedHtml.replace("__POSTERHASH__", StringEscapeUtils.escapeXml10(coverImageIpfs));
-			embedHtml=embedHtml.replace("__VIDEOHASH__", StringEscapeUtils.escapeXml10(videoIpfs));
-			if (cache.size()>100) {
+			embedHtml = embedHtml.replace("__TITLE__", StringEscapeUtils.escapeXml10(post.getTitle()));
+			embedHtml = embedHtml.replace("__POSTERHASH__", StringEscapeUtils.escapeXml10(coverImageIpfs));
+			embedHtml = embedHtml.replace("__VIDEOHASH__", StringEscapeUtils.escapeXml10(videoIpfs));
+			if (cache.size() > 100) {
 				cache.clear();
 			}
 			cache.put(key, embedHtml);
 			return embedHtml;
 		}
-		
+
 		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		return "NOT FOUND";
-		
+
 	}
-	
+
 }
