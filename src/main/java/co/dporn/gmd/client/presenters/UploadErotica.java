7ea61cb6ead192eb91677698c522b4f24b757f95
Leather Dog Muksihs <muksihs@gmail.com>
2018-11-12 12:01:19 -0500

    initial view and presenter design

---
+++ src/main/java/co/dporn/gmd/client/presenters/UploadErotica.java
@@ -0,0 +1,22 @@
+package co.dporn.gmd.client.presenters;
+
+import java.util.List;
+
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+
+import co.dporn.gmd.client.presenters.UploadErotica.UploadEroticaView;
+import co.dporn.gmd.client.views.CommonUploadViewFeatures;
+import co.dporn.gmd.client.views.IsView;
+
+public interface UploadErotica extends IsPresenter<UploadEroticaView>, ScheduledCommand {
+	interface UploadEroticaView extends IsView<UploadErotica>, CommonUploadViewFeatures {
+		
+	}
+	void selectStockCoverImage(String coverImage);
+	void showAvailableStockCoverImages();
+	void submit();
+	void cancel();
+	void reset();
+	void previousTagSet();
+	void useTagSet(List<String> tags);
+}
