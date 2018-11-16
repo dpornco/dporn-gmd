1d642c9a1ad3fcd560acc0a689e3dfabb7037008
Leather Dog Muksihs <muksihs@gmail.com>
2018-11-15 23:35:16 -0500

    cache authorization lookups for 30 minutes

--- src/main/java/co/dporn/gmd/servlet/ServerSteemConnect.java
+++ src/main/java/co/dporn/gmd/servlet/ServerSteemConnect.java
@@ -1,13 +1,22 @@
 package co.dporn.gmd.servlet;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class ServerSteemConnect {
-	private static final String endPoint = "https://steemconnect.com/api/";
+import org.apache.commons.collections4.map.LRUMap;
+import org.apache.commons.collections4.map.PassiveExpiringMap;
 
+public class ServerSteemConnect {
+	private static final long MINUTE_ms = 1000l * 60;
+	private static final String endPoint = "https://steemconnect.com/api/";
+	private static final Map<String, String> USERNAME_CACHE = Collections.synchronizedMap(new PassiveExpiringMap<>(30*MINUTE_ms, new LRUMap<>(256)));
 	public static String username(String authorization) {
+		String cached = USERNAME_CACHE.get(authorization);
+		if (cached!=null) {
+			return cached;
+		}
 		Map<String, String> queryParams = new LinkedHashMap<>();
 		queryParams.put("access_token", authorization);
 		String result = ServerRestClient.get(endPoint + "me", queryParams);
@@ -17,6 +26,8 @@ public class ServerSteemConnect {
 		} catch (IOException e) {
 			return "";
 		}
-		return me.getUser();
+		String user = me.getUser();
+		USERNAME_CACHE.put(authorization, user);
+		return user;
 	}
 }
