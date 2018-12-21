cb147d18813056aecebb1ef52428ab4c58ca44ab
Leather Dog Muksihs <muksihs@gmail.com>
2018-12-20 22:39:32 -0500

    fix test

--- src/test/java/manual/ManualTestsSteemJ.java
+++ src/test/java/manual/ManualTestsSteemJ.java
@@ -1,18 +1,19 @@
 package manual;
 
 import java.util.List;
+import java.util.Set;
 
 import co.dporn.gmd.servlet.utils.steemj.SteemJInstance;
 
 public class ManualTestsSteemJ {
 
 	public static void main(String[] args) {
-		List<String> verified = SteemJInstance.get().getNsfwVerifiedList();
+		Set<String> verified = SteemJInstance.get().getDpornVerifiedSet();
 		for (String name: verified) {
 			System.out.println("V: "+name);
 		}
 		System.out.println("TOTAL: "+verified.size());
-		List<String> active = SteemJInstance.get().getActiveNsfwVerifiedList();
+		List<String> active = SteemJInstance.get().getActiveDpornVerifiedList();
 		for (String name: active) {
 			System.out.println("A: "+name);
 		}
