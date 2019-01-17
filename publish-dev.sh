760a3ef7dbda0bdaa1e6f39b16ca18dd43ba850c
Leather Dog Muksihs <muksihs@gmail.com>
2019-01-16 23:02:44 -0500

    tomcat8

--- publish-dev.sh
+++ publish-dev.sh
@@ -3,28 +3,21 @@
 set -e
 set -o pipefail
 
+export SOURCE="./build/libs/dporn-gmd.war"
+export DEST="/var/lib/tomcat8/webapps/dev-dporn-gmd.war"
+
 cd "$(dirname "$0")"
 
 ./gradlew clean
 ./gradlew build
-./gradlew buildProductDev
 
 . ./tunnel.sh
 
-cd build/output
+cd 
 
-#do includes *before* excludes
-
-echo "STOP REMOTE SERVICES"
-ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; bash stop.sh' || true
-#recursive rsync
-rsync	--exclude /work/ \
-		--exclude /temp/ \
-		--progress --verbose --human-readable --compress --delete-before -a \
+#single file rsync directly to tomcat webapps folder, must be group member of tomcat8
+rsync	--progress --verbose --human-readable --compress --delete-before -a \
 		-e "ssh -p $PORT" \
-	Dev/ "$USER"@"$HOST":Dev/
+	"$SOURCE" "$USER"@"$HOST":"$DEST"
 	
-echo "START REMOTE SERVICES"
-ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; (bash start.sh) < /dev/null > tomcat.log 2> tomcat.err & sleep 6' || true
-ssh -p "$PORT" "$USER"@"$HOST" 'cd Dev; (bash restart.sh || bash start.sh) < /dev/null > tomcat.log 2> tomcat.err & sleep 3'
 echo "DONE"
